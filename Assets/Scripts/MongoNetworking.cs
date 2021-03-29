32275d8a1bbc80da145a7d66312a3649b7068968
coserra <81561717+coserra@users.noreply.github.com>
2021-03-29 12:57:50 +0200

    Subiendo proyecto
    Subiendo el proyecto, con los mapas cargados.

---
+++ Assets/Scripts/MongoNetworking.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MongoNetworking : MonoBehaviour
+{
+    MongoClient client = new MongoClient("mongodb+srv://admin:admin@test.7yomk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    IMongoDatabase database;
+    IMongoCollection<BsonDocument> collection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        database = client.GetDatabase("sample_geospatial");
+        collection = database.GetCollection<BsonDocument>("shipwrecks");
+
+        var document = new BsonDocument { { "juan", "22" } };
+        collection.InsertOne(document);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
